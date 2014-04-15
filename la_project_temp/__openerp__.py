48e875a54d9f790e73ab489b37e5751b25b675f1
cysnake4713 <cysnake4713@gmail.com>
2014-04-15 18:48:38 +0800

    project dome 完成

---
+++ la_project_temp/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+# __author__ = cysnake4713@gmail.com
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Project Module For Leiao Company",
+    "version": "0.1",
+    "author": "cysnake4713",
+    "maintainer": "cysnake4713@gmail.com",
+    "website": "http://www.cysnake.com",
+    "description": u"""
+Project Module For Leiao Company
+""",
+    "depends": ['base','l10n_cn_honey','mail'],
+    "category": "Project",
+    "demo_xml": [],
+    "data": [
+    ],
+    "update_xml": [
+        'project.xml',
+    ],
+    "init_xml": [
+
+    ],
+    "css": [
+    ],
+    "license": "GPL-3",
+    "active": False,
+    "installable": True
+}
