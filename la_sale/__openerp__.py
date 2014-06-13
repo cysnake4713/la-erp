10a35d5c6f72531c960cd70300519b14bb51fb39
cysnake4713 <cysnake4713@gmail.com>
2014-06-13 23:45:33 +0800

    LA-2 saleevent 权限完成

--- la_sale/__openerp__.py
+++ la_sale/__openerp__.py
@@ -5,7 +5,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,6 +29,8 @@ Project Module For Leiao Company
     'category': 'Project Management',
     'demo_xml': [],
     'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/res_partner.xml',
         'views/sale_menu.xml',
         'views/event_view.xml',
