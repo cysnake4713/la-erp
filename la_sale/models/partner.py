5bc3c46cea904dfb17d9490935153dad421a7680
cysnake4713 <cysnake4713@gmail.com>
2014-07-07 22:20:34 +0800

    [imp]修改甲方联系人信息显示

--- la_sale/models/partner.py
+++ la_sale/models/partner.py
@@ -4,7 +4,7 @@ from openerp.osv import osv, fields
 from openerp import SUPERUSER_ID
 
 
-class ParnterInherit(osv.Model):
+class PartnerInherit(osv.Model):
     _inherit = "res.partner"
 
     def _get_related_projects(self, cr, uid, ids, field_name, arg, context):
@@ -27,3 +27,25 @@ class ParnterInherit(osv.Model):
         'related_projects_number': fields.function(_get_related_projects, type='char', string='Related Projects Number',
                                                    multi='project'),
     }
+
+    def name_get(self, cr, user, ids, context=None):
+        """Returns the preferred display value (text representation) for the records with the
+           given ``ids``. By default this will be the value of the ``name`` column, unless
+           the model implements a custom behavior.
+           Can sometimes be seen as the inverse function of :meth:`~.name_search`, but it is not
+           guaranteed to be.
+
+           :rtype: list(tuple)
+           :return: list of pairs ``(id,text_repr)`` for all records with the given ``ids``.
+        """
+        if not ids:
+            return []
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+
+        if self._rec_name in self._all_columns:
+            rec_name_column = self._all_columns[self._rec_name].column
+            return [(r['id'], rec_name_column.as_display_name(cr, user, self, r[self._rec_name], context=context))
+                        for r in self.read(cr, user, ids, [self._rec_name],
+                                       load='_classic_write', context=context)]
+        return [(id, "%s,%s" % (self._name, id)) for id in ids]
