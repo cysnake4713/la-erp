886ddab3585fddf11b0060d766972025f7918bab
cysnake4713 <cysnake4713@gmail.com>
2015-03-24 17:58:19 +0800

    [IMP]更新依赖

--- addons/la_project/__openerp__.py
+++ addons/la_project/__openerp__.py
@@ -25,7 +25,7 @@
     'description': u"""
 Project Module For Leiao Company
 """,
-    'depends': ['base', 'base_override'],
+    'depends': ['base'],
     'category': 'Project Management',
     'demo_xml': [],
     'data': [
